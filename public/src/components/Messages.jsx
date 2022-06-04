baf5c4cbcf174582603cd36443582cf3cc2a389b
SourabhSisodia <sourabhsisodia010@gmail.com>
2022-06-05 01:16:23 +0530

    first commit

---
+++ public/src/components/Messages.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+function Messages() {
+  return (
+    <Container>
+      <div>Messages</div>
+    </Container>
+  );
+}
+const Container = styled.div`
+  height: 80%;
+`;
+export default Messages;
